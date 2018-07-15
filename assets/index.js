554a806a76f0e1fee07a1a09d9cb45e2988732ee
nurul0727 <nurul.anwar.dev@gmail.com>
2018-07-15 00:57:29 -0800

    added assets

---
+++ assets/index.js
@@ -0,0 +1,77 @@
+import heartOutline from './heart-outline.png';
+import heart from './heart.png';
+import search from './Search.png';
+import tick from './tick.png';
+import cross from './cross.png';
+import logo02 from './logo02.png';
+import logo from './logo.png';
+import menu from './menu.png';
+import discord from './discord.png';
+import github from './github.png';
+import telegram from './telegram.png';
+import twitter from './twitter.png';
+import upload from './upload.png';
+import creator1 from './creator1.png';
+import creator2 from './creator2.png';
+import creator3 from './creator3.png';
+import creator4 from './creator4.png';
+import creator5 from './creator5.png';
+import creator6 from './creator6.png';
+import creator7 from './creator7.png';
+import creator8 from './creator8.png';
+import creator9 from './creator9.png';
+import creator10 from './creator10.jpg';
+import right from './right.png';
+import left from './left.png';
+import headerbg from './headerbg.png';
+import loader from './loader.gif';
+import arrow from './arrow.png';
+import youtube from './youtube.png';
+import mainImage1 from './main/image1.png';
+import mainImage2 from './main/image2.png';
+import mainImage3 from './main/image3.png';
+import mainNFT1 from './main/nft1.png';
+import mainNFT2 from './main/nft2.png';
+import mainNFT3 from './main/nft3.png';
+import mainNFT4 from './main/nft4.png';
+import instagram from './instagram.png';
+
+export default {
+  heartOutline,
+  heart,
+  search,
+  tick,
+  cross,
+  logo02,
+  logo,
+  menu,
+  discord,
+  github,
+  telegram,
+  twitter,
+  upload,
+  creator1,
+  creator2,
+  creator3,
+  creator4,
+  creator5,
+  creator6,
+  creator7,
+  creator8,
+  creator9,
+  creator10,
+  right,
+  left,
+  headerbg,
+  loader,
+  arrow,
+  youtube,
+  mainImage1,
+  mainImage2,
+  mainImage3,
+  mainNFT1,
+  mainNFT2,
+  mainNFT3,
+  mainNFT4,
+  instagram,
+};
